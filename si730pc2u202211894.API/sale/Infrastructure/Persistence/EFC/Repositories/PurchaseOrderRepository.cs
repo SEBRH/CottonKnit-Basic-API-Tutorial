638a062438f56367bbe0d5b363ace2ecaecd7163
SEBRH <u202211894@upc.edu.pe>
2024-06-14 21:01:59 -0500

    feat(Services): Implemented the Repository for the PurchaseOrderAggregate.

---
+++ si730pc2u202211894.API/sale/Infrastructure/Persistence/EFC/Repositories/PurchaseOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using si730pc2u202211894.API.sale.Domain.Models.Aggregates;
+using si730pc2u202211894.API.sale.Domain.Models.ValueObjects;
+using si730pc2u202211894.API.sale.Domain.Repositories;
+using si730pc2u202211894.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using si730pc2u202211894.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace si730pc2u202211894.API.sale.Infrastructure.Persistence.EFC.Repositories;
+
+public class PurchaseOrderRepository(AppDbContext context): BaseRepository<PurchaseOrder>(context), IPurchaseOrderRepository 
+{
+    public async Task<bool> ExistsByCustomerAndFabricId(string customer, int fabricId)
+    {
+        return await context.Set<PurchaseOrder>().AnyAsync(x => x.Customer == customer && x.FabricId == (EFabricType)fabricId);
+    }
+}
